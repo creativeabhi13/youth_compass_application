46c78c88a87a94e3d9a767f4d3fd9e740c0a6345
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-03-13 16:03:33 +0530

    Update Student.dart

--- lib/Pages/Student.dart
+++ lib/Pages/Student.dart
@@ -10,10 +10,12 @@ class _Student extends State<Student>
   @override
   Widget build(BuildContext context)
   {
+
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child:Column(
+        child: SingleChildScrollView(child: Column(
           children:[
             Padding(
               padding:const EdgeInsets.all(12.0) ,
@@ -28,7 +30,7 @@ class _Student extends State<Student>
             Padding(
               padding: const EdgeInsets.all(18.0),
               child:Text(
-                "List of Student",
+                "Add  Student",
                 style:TextStyle(
                   color:Colors.white,
                   fontSize: 30.0,
@@ -37,8 +39,142 @@ class _Student extends State<Student>
                 textAlign: TextAlign.start,
               ),
             ),
+            Padding(
+                padding: EdgeInsets.all(40.0),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.name,
+                        onChanged: (value) {
+                          setState(() {
+
+                          });
+                        },
+                       /* onSubmitted: (value) =>
+                            convert(double.parse(value), _time), */
+                        decoration: InputDecoration(
+                          hintText: 'Name of the Student',
+                          icon: Icon(Icons.person,color: Colors.amber,size: 45.0,),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.name,
+                        onChanged: (value) {
+                          setState(() {
+
+                          });
+                        },
+                        /* onSubmitted: (value) =>
+                            convert(double.parse(value), _time), */
+                        decoration: InputDecoration(
+                          hintText: 'Name of the Schools',
+                          icon: Icon(Icons.school_sharp,color: Colors.amber,size: 45.0,),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {
+                          setState(() {
+
+                          });
+                        },
+                        /* onSubmitted: (value) =>
+                            convert(double.parse(value), _time), */
+                        decoration: InputDecoration(
+                          hintText: 'Roll Number',
+                          icon: Icon(Icons.numbers,color: Colors.amber,size: 45.0,),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 70,
+                      ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                      /*  convert(_power, _time);*/
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(size.width*0.58, size.height*0.09),
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                          BorderRadius.circular(10), // <-- Radius
+                        ),
+                      ),
+                      child: const Text(
+                        'Add Student',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                    ],
+                  ),
+                ),
+              ),
+                )
           ],
         ),
+      ),
       ),
     );
   }
