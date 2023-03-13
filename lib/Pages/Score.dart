df4813b8e228f0ab1adf6199f9c2e250c747a7a4
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-03-13 16:04:33 +0530

    Update Score.dart

--- lib/Pages/Score.dart
+++ lib/Pages/Score.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Student extends StatefulWidget {
-  const Student({Key ? key}) : super(key:key);
+class Score extends StatefulWidget {
+  const Score({Key ? key}) : super(key:key);
   @override
-  State<Student>createState() =>_Student();
+  State<Score>createState() =>_Score();
 }
-class _Student extends State<Student>
+class _Score extends State<Score>
 {
   @override
   Widget build(BuildContext context)
   {
-
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: SingleChildScrollView(child: Column(
+        child:SingleChildScrollView(child:Column(
           children:[
             Padding(
               padding:const EdgeInsets.all(12.0) ,
@@ -30,7 +29,7 @@ class _Student extends State<Student>
             Padding(
               padding: const EdgeInsets.all(18.0),
               child:Text(
-                "Add  Student",
+                "Score Card",
                 style:TextStyle(
                   color:Colors.white,
                   fontSize: 30.0,
@@ -40,7 +39,7 @@ class _Student extends State<Student>
               ),
             ),
             Padding(
-                padding: EdgeInsets.all(40.0),
+              padding: EdgeInsets.all(40.0),
               child: Center(
                 child: SingleChildScrollView(
                   child: Column(
@@ -53,7 +52,7 @@ class _Student extends State<Student>
 
                           });
                         },
-                       /* onSubmitted: (value) =>
+                        /* onSubmitted: (value) =>
                             convert(double.parse(value), _time), */
                         decoration: InputDecoration(
                           hintText: 'Name of the Student',
@@ -113,6 +112,39 @@ class _Student extends State<Student>
                         height: 30,
                       ),
                       TextField(
+                        keyboardType: TextInputType.name,
+                        onChanged: (value) {
+                          setState(() {
+
+                          });
+                        },
+                        /* onSubmitted: (value) =>
+                            convert(double.parse(value), _time), */
+                        decoration: InputDecoration(
+                          hintText: 'Area of the Schools',
+                          icon: Icon(Icons.location_city_sharp,color: Colors.amber,size: 45.0,),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
                         keyboardType: TextInputType.number,
                         onChanged: (value) {
                           setState(() {
@@ -122,8 +154,8 @@ class _Student extends State<Student>
                         /* onSubmitted: (value) =>
                             convert(double.parse(value), _time), */
                         decoration: InputDecoration(
-                          hintText: 'Roll Number',
-                          icon: Icon(Icons.numbers,color: Colors.amber,size: 45.0,),
+                          hintText: 'Score obtained',
+                          icon: Icon(Icons.score_outlined,color: Colors.amber,size: 45.0,),
                           hintStyle: const TextStyle(color: Colors.white),
                           enabledBorder: OutlineInputBorder(
                             borderSide:
@@ -145,33 +177,34 @@ class _Student extends State<Student>
                       const SizedBox(
                         height: 70,
                       ),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                      /*  convert(_power, _time);*/
-                      },
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: Size(size.width*0.58, size.height*0.09),
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                          BorderRadius.circular(10), // <-- Radius
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            /*  convert(_power, _time);*/
+                          },
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(size.width*0.58, size.height*0.09),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.circular(10), // <-- Radius
+                            ),
+                          ),
+                          child: const Text(
+                            'Add Score',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
                         ),
                       ),
-                      child: const Text(
-                        'Add Student',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                  ),
                     ],
                   ),
                 ),
               ),
-                )
+            )
+
           ],
         ),
       ),
