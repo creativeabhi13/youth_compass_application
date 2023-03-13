163481f9e5710888848344c2b864285bc3a1b103
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-03-13 16:02:03 +0530

    Update School.dart

--- lib/Pages/School.dart
+++ lib/Pages/School.dart
@@ -10,10 +10,11 @@ class _School extends State<School>
   @override
   Widget build(BuildContext context)
   {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child:Column(
+        child:SingleChildScrollView(child: Column(
           children:[
             Padding(
               padding:const EdgeInsets.all(12.0) ,
@@ -28,7 +29,7 @@ class _School extends State<School>
             Padding(
               padding: const EdgeInsets.all(18.0),
               child:Text(
-                "List of School",
+                "Add School",
                 style:TextStyle(
                   color:Colors.white,
                   fontSize: 30.0,
@@ -37,8 +38,143 @@ class _School extends State<School>
                 textAlign: TextAlign.start,
               ),
             ),
+            Padding(
+              padding: EdgeInsets.all(40.0),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.name,
+                        onChanged: (value) {
+                          setState(() {
+
+                          });
+                        },
+                        /* onSubmitted: (value) =>
+                            convert(double.parse(value), _time), */
+                        decoration: InputDecoration(
+                          hintText: 'Name of the School',
+                          icon: Icon(Icons.school_sharp,color: Colors.amber,size: 45.0,),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.name,
+                        onChanged: (value) {
+                          setState(() {
+
+                          });
+                        },
+                        /* onSubmitted: (value) =>
+                            convert(double.parse(value), _time), */
+                        decoration: InputDecoration(
+                          hintText: 'city of school',
+                          icon: Icon(Icons.location_city,color: Colors.amber,size: 45.0,),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {
+                          setState(() {
+
+                          });
+                        },
+                        /* onSubmitted: (value) =>
+                            convert(double.parse(value), _time), */
+                        decoration: InputDecoration(
+                          hintText: 'School Id',
+                          icon: Icon(Icons.format_list_numbered_rtl_sharp,color: Colors.amber,size: 45.0,),
+                          hintStyle: const TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide:
+                            const BorderSide(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 70,
+                      ),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            /*  convert(_power, _time);*/
+                          },
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: Size(size.width*0.58, size.height*0.09),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.circular(10), // <-- Radius
+                            ),
+                          ),
+                          child: const Text(
+                            'Add Schools',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+
           ],
         ),
+      ),
       ),
     );
   }
