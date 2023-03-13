d1edfe2849da05e5ceda43228b1ff1063541a320
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2023-03-13 12:34:17 +0530

    hi

--- lib/Pages/Admin.dart
+++ lib/Pages/Admin.dart
@@ -37,9 +37,171 @@ class _Admin extends State<Admin>
                     textAlign: TextAlign.start,
                   ),
                 ),
+
+  Padding(
+  padding:const EdgeInsets.all(12.0),
+  child:Center(
+  child:Wrap(
+  spacing: 20.0,
+  runSpacing: 20.0,
+  children: [
+  GestureDetector(
+  onTap: (){
+  setState(() {
+  Navigator.pushReplacement(
+  context, MaterialPageRoute(builder: (_) => Admin()));
+  });
+  },
+  child: SizedBox(
+  width:160.0 ,
+  height:160.0,
+  child:Card(
+  color:Color.fromARGB(255, 21, 21, 21),
+  elevation:2.0,
+  shape: RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(8.0),
+  ),
+  child: Center(
+  child:Padding(
+  padding: const EdgeInsets.all(8.0),
+  child:Column(
+
+  children: [
+  Image.asset("assets/Admin.png",width: 64.0,),
+  SizedBox(height:16.0),
+  Text("Admin",style:TextStyle(
+  color:Colors.white,
+  fontWeight:FontWeight.bold,
+  fontSize:20.0,
+  )),
+  SizedBox(height: 5.0),
+  Text("1 Items",style: TextStyle(
+  color: Colors.white,
+
+  ),)
+  ],
+  ) ,
+  ) ,
+  ),
+    )
+    ),
+    ),
+
           ],
           ),
         ),
+        ),
+
+  Padding(
+  padding:const EdgeInsets.all(12.0),
+  child:Center(
+  child:Wrap(
+  spacing: 20.0,
+  runSpacing: 20.0,
+  children: [
+  GestureDetector(
+  onTap: (){
+  setState(() {
+  Navigator.pushReplacement(
+  context, MaterialPageRoute(builder: (_) => Admin()));
+  });
+  },
+  child: SizedBox(
+  width:160.0 ,
+  height:160.0,
+  child:Card(
+  color:Color.fromARGB(255, 21, 21, 21),
+  elevation:2.0,
+  shape: RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(8.0),
+  ),
+  child: Center(
+  child:Padding(
+  padding: const EdgeInsets.all(8.0),
+  child:Column(
+
+  children: [
+  Image.asset("assets/Admin.png",width: 64.0,),
+  SizedBox(height:16.0),
+  Text("Sub Admin",style:TextStyle(
+  color:Colors.white,
+  fontWeight:FontWeight.bold,
+  fontSize:20.0,
+  )),
+  SizedBox(height: 5.0),
+  Text("1 Items",style: TextStyle(
+  color: Colors.white,
+
+  ),)
+  ],
+  ) ,
+  ) ,
+  ),
+    )
+    ),
+    ),
+
+
+          ]
+       )
+        )
+  ),
+  Padding(
+  padding:const EdgeInsets.all(12.0),
+  child:Center(
+  child:Wrap(
+  spacing: 20.0,
+  runSpacing: 20.0,
+  children: [
+  GestureDetector(
+  onTap: (){
+  setState(() {
+  Navigator.pushReplacement(
+  context, MaterialPageRoute(builder: (_) => Admin()));
+  });
+  },
+  child: SizedBox(
+  width:160.0 ,
+  height:160.0,
+  child:Card(
+  color:Color.fromARGB(255, 21, 21, 21),
+  elevation:2.0,
+  shape: RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(8.0),
+  ),
+  child: Center(
+  child:Padding(
+  padding: const EdgeInsets.all(8.0),
+  child:Column(
+
+  children: [
+  Image.asset("assets/Admin.png",width: 64.0,),
+  SizedBox(height:16.0),
+  Text("Volunteer",style:TextStyle(
+  color:Colors.white,
+  fontWeight:FontWeight.bold,
+  fontSize:20.0,
+  )),
+  SizedBox(height: 5.0),
+  Text("3 Items",style: TextStyle(
+  color: Colors.white,
+
+  ),)
+  ],
+  ) ,
+  ) ,
+  ),
+    )
+    ),
+    ),
+    ]
+    )
+        ),
+  ),
+        ]
+    )
+        )
+
       );
   }
 }
